cabcd292bf258f8cd2a21238492e2cd8d56c0c98
olawaleraji <savagedotjsx@gmail.com>
2024-09-03 17:44:47 +0100

    first commit

--- Teni's Portfolio/src/Contact.jsx
+++ Teni's Portfolio/src/Contact.jsx
@@ -30,24 +30,6 @@ export default () => {
               />
             </div>
             <div>
-              <label className='font-medium'>Phone number</label>
-              <div className='relative mt-2'>
-                <div className='absolute inset-y-0 left-3 my-auto h-6 flex items-center border-r pr-2'>
-                  <select className='text-sm bg-transparent outline-none rounded-lg h-full'>
-                    <option>US</option>
-                    <option>ES</option>
-                    <option>MR</option>
-                  </select>
-                </div>
-                <input
-                  type='number'
-                  placeholder='+1 (555) 000-000'
-                  required
-                  className='w-full pl-[4.5rem] pr-3 py-2 appearance-none bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg'
-                />
-              </div>
-            </div>
-            <div>
               <label className='font-medium'>Message</label>
               <textarea
                 required
