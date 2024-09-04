3e1ae893a1f40c9c6511af7f47d0f33423a5bc49
olawaleraji <savagedotjsx@gmail.com>
2024-09-04 03:33:45 +0100

    first commit

--- Teni's Portfolio/src/Build.jsx
+++ Teni's Portfolio/src/Build.jsx
@@ -3,30 +3,44 @@ import React from 'react'
 const CollaborationSection = () => {
   return (
     <section className='relative overflow-hidden py-28 px-4 bg-gray-900 md:px-8'>
-      <div className='w-full h-full rounded-full bg-gradient-to-r from-[#58AEF1] to-pink-500 absolute -top-12 -right-14 blur-2xl opacity-10'></div>
-      <div className='max-w-xl mx-auto text-center relative'>
+      <div className='w-[500px] h-[500px] rounded-full bg-gradient-to-r from-[#58AEF1] via-purple-500 to-pink-500 absolute -top-12 -right-14 blur-2xl opacity-20'></div>
+      <div className='max-w-xl mx-auto text-center relative z-10'>
         <div className='py-4'>
           <h3 className='text-3xl text-gray-200 font-semibold md:text-4xl'>
-            Let's Collaborate!
+            <span className='fade-slide-in animation-delay-0 block'>Let's</span>
+            <span className='fade-slide-in animation-delay-1000 block'>
+              Collaborate!
+            </span>
           </h3>
           <p className='text-gray-300 leading-relaxed mt-3'>
-            I am passionate about building innovative products and continually
-            sharpening my development skills. If you’re looking for a dedicated
-            and enthusiastic collaborator, I’d love the opportunity to work with
-            you on your projects. Together, we can create something amazing and
-            push the boundaries of technology.
+            <span className='fade-slide-in animation-delay-2000 block'>
+              I am passionate about building innovative products
+            </span>
+            <span className='fade-slide-in animation-delay-3000 block'>
+              and continually sharpening my development skills.
+            </span>
+            <span className='fade-slide-in animation-delay-4000 block'>
+              If you’re looking for a dedicated and enthusiastic collaborator,
+            </span>
+            <span className='fade-slide-in animation-delay-5000 block'>
+              I’d love the opportunity to work with you on your projects.
+            </span>
+            <span className='fade-slide-in animation-delay-6000 block'>
+              Together, we can create something amazing and push the boundaries
+              of technology.
+            </span>
           </p>
         </div>
         <div className='mt-5 items-center justify-center gap-3 sm:flex'>
           <a
             href='#contact'
-            className='block w-full mt-2 py-2.5 px-8 text-gray-700 bg-white rounded-md duration-150 hover:bg-gray-100 sm:w-auto'
+            className='block w-full mt-2 py-2.5 px-8 text-gray-700 bg-white rounded-md shadow-md duration-150 hover:bg-gray-100 sm:w-auto fade-slide-in animation-delay-7000'
           >
             Get in Touch
           </a>
           <a
             href='#products'
-            className='block w-full mt-2 py-2.5 px-8 text-gray-300 bg-gray-700 rounded-md duration-150 hover:bg-gray-800 sm:w-auto'
+            className='block w-full mt-2 py-2.5 px-8 text-gray-300 bg-gradient-to-r from-gray-700 to-gray-800 rounded-md shadow-md duration-150 hover:bg-gray-900 sm:w-auto fade-slide-in animation-delay-8000'
           >
             View My Work
           </a>
--- Teni's Portfolio/src/Hero.jsx
+++ Teni's Portfolio/src/Hero.jsx
@@ -6,14 +6,18 @@ export default () => {
       <section className='pt-24 mx-auto max-w-screen-xl pb-12 px-4 items-center lg:flex md:px-8'>
         <div className='space-y-4 flex-1 sm:text-center lg:text-left'>
           <h1 className='text-white font-bold text-4xl xl:text-5xl'>
-            Hi,
-            <span className='text-indigo-400'> I'm TENIOLA JIMOH</span>
-            <br />
-            an entry-level
-            <br />
-            SOFTWARE DEVELOPER
+            <span className='block fadeIn animation-delay-0'>Hi,</span>
+            <span className='text-indigo-400 block fadeIn animation-delay-500'>
+              I'm TENIOLA JIMOH
+            </span>
+            <span className='block fadeIn animation-delay-1000'>
+              an entry-level
+            </span>
+            <span className='block fadeIn animation-delay-1500'>
+              SOFTWARE DEVELOPER
+            </span>
           </h1>
-          <p className='text-gray-300 max-w-xl leading-relaxed sm:mx-auto lg:ml-0'>
+          <p className='text-gray-300 max-w-xl leading-relaxed sm:mx-auto lg:ml-0 fadeIn animation-delay-2000'>
             A highly motivated student studying software engineering but
             currently still in frontend engineering. Actively seeking
             opportunities to apply my skills and knowledge to real-world
@@ -22,7 +26,7 @@ export default () => {
           <div className='pt-10 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start'>
             <a
               href='#products'
-              className='px-7 py-3 w-full font-semibold bg-white text-gray-800 text-center rounded-md shadow-md block sm:w-auto'
+              className='px-7 py-3 w-full font-semibold bg-white text-gray-800 text-center rounded-md shadow-md block sm:w-auto fadeIn animation-delay-2500'
             >
               View My Work
             </a>
@@ -31,7 +35,7 @@ export default () => {
         <div className='flex-1 text-center mt-7 lg:mt-0 lg:ml-3'>
           <img
             src={Tennie}
-            className='w-full mx-auto sm:w-10/12 lg:w-full rounded-lg h-[28rem]'
+            className='w-full mx-auto sm:w-10/12 lg:w-full rounded-lg h-[28rem] fadeIn animation-delay-3000'
             alt='Illustration'
           />
         </div>
